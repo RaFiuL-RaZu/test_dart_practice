638cfd9e871c4d3e115cc84f7e6e3e493f8b7c3b
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-14 21:58:53 +0600

    init commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,12 +1,7 @@
 
 void main() {
-  var a=20;
-  var b=10;
+  var a=25;
+  var b=20;
 
-  if(a%2!=0 || b%3==0){
-    print("True");
-  }
-  else{
-    print("False");
-  }
+  print("a & b=${a&b}");
 }

8de49a8138ae25cdb8dff0a93d1148f338506987
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-17 22:03:49 +0600

    init commit

--- bin/test.dart
+++ bin/test.dart
@@ -2,13 +2,10 @@
 
 void main() {
 
-  var city=['Dhaka','Rangpur','Kurigram','Bogura','Dinajpur'];
+  const city=['Dhaka','Rangpur','Kurigram','Bogura','Dinajpur'];
 
   print(city);
-  city.add("Cumilla");
-  city.remove("Dhaka");
 
-  print(city);
 
 
 

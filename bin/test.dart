3548d734e21eaebe035108e09510b0f169f8a282
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-17 11:35:47 +0600

    init commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,8 +1,8 @@
 
 
 void main() {
-  var city=['Dhaka','Rangpur','Barishal','Khulna','Kurigram',20,25,30,true];
+  var person={'Name':'Rafiul', 'Age':23, 'Branch':'Computer'};
 
-  print(city[8]);
+  print(person);
 
 }

75f19c97b7308bce8785d57f100bf17839cba099
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-17 11:24:35 +0600

    init commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,23 +1,7 @@
 
 
-
 void main() {
-  String str1='Welcome to dart';
-  String str2=" I am Razu";
-
-  String str3=str1+str2;
-  print(str3);
-
-  var a=26;
-  var b=10;
-  print("This result is=${a%b}");
-
-  var name="Razu";
-  var roll=15;
-
-  print("My name is ${name}, My roll is ${roll}.");
-
-
+  var city=('Dhaka','Rangpur','Barishal','Khulna','Kurigram');
+  print(city);
 
 }
-

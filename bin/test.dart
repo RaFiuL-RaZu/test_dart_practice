f205b27aa9cef1ff2af864b95c4ff783ebd947b5
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-18 12:53:12 +0600

    init commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,11 +1,11 @@
 
  void main(){
 
-  var x=20;
+  var x=35;
   var y=25;
 
   if(x>y){
-    print("X Is Gratter Than");
+    print("X Is Gratter Than Y");
   }
   else{
     print("Y Is Gratter Than X");

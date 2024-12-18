165ca4c4145eeac15e75331173d5beb2e7cad4ec
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-18 11:55:52 +0600

    init commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,54 +1,10 @@
 
-/*
-void main() {
-  var myCityset=<String>{'Dhaka','Barishal','Khulna','Rangpur','Kurigram'};
+ void main(){
 
- myCityset.addAll({"Lalmoni","Shylet",});
-
-print(myCityset);
-}
-
-
-
- */
-
-/*
-void main(){
-
-  var myCityset=<String>{'Dhaka','Barishal','Khulna','Rangpur','Kurigram'};
-
-  myCityset.addAll({"Lalmoni","Shylet",});
-
-  print(myCityset.elementAt(6));
-
-}
-
-
- */
-
-/*
-void main() {
-  var myCityset = <String>{'Dhaka', 'Barishal', 'Khulna', 'Rangpur', 'Kurigram'};
-
-  myCityset.clear();
-
-  print(myCityset);
-}
-
- */
-
-
-
-void main() {
-  var myCityset = <String>{'Dhaka', 'Barishal', 'Khulna', 'Rangpur', 'Kurigram'};
-
-print(myCityset.first);
-print(myCityset.isEmpty);
-print(myCityset.isNotEmpty);
-print(myCityset.last);
-print(myCityset.length);
-print(myCityset.hashCode);
-}
-
-
-
+  var person={
+    'Name':'Razu',
+    'Age':24,
+    'Branch':'Computer'
+  };
+  print(person);
+ }

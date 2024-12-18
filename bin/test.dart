d39ab0ed1b70ff67b6609a3a6a39ba3a08e7882b
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-18 19:05:02 +0600

    init commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,9 +1,17 @@
 
  void main(){
 
-var alphabetList={'A','B','c','D','E','F'};
-for(var onealpha in alphabetList){
- print(onealpha);
-}
+var productList=[
+  {'product':'Soap','price':100},
+  {'product':'Sugar','price':120},
+  {'product':'milk','price':80},
+  {'product':'sempo','price':200},
+  {'product':'potato','price':70},
+  {'product':'rice','price':50},
+  {'product':'biscuit','price':40},
+ ];
+ for(var oneproduct in productList){
+  print(oneproduct);
+  }
 
  }

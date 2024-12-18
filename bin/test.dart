ce5e6e349dc06711c87a2622a54fab8df1a437c8
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-18 12:41:37 +0600

    init commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,14 +1,9 @@
 
  void main(){
 
-  var person=new Map();
-      person['Name']='Rafiul';
-  person['Roll']='10';
-  person['Class']='Diploma';
-  person['Dept']='Computer';
-  person['CGPA']='3.88';
-  person.remove("Roll");
-
-  print(person);
+  var number=40;
+  if(number>30){
+    print("Big");
+  }
 
  }

119b956cc25a7686cbfc50176ec97f7994f564cf
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-19 10:33:51 +0600

    init commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,7 +1,12 @@
 
  void main(){
 
- var name='Razu';
- print(name);
+ var x=15;
+ var y=10;
 
+ print("Additon Number =${x+y}");
+ print("Subtract Number =${x-y}");
+ print("Multiple Number =${x*y}");
+ print("Divide Number =${x/y}");
+ print("Modulous Number =${x%y}");
  }

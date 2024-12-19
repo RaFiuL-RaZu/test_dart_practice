6c5473b7a25075743bd92f24350cd804074e07cb
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-19 10:40:27 +0600

    init commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,12 +1,7 @@
 
  void main(){
+ String numberString="123";
+ int number=int.parse(numberString);
+ print(number);
 
- var x=15;
- var y=10;
-
- print("Additon Number =${x+y}");
- print("Subtract Number =${x-y}");
- print("Multiple Number =${x*y}");
- print("Divide Number =${x/y}");
- print("Modulous Number =${x%y}");
  }

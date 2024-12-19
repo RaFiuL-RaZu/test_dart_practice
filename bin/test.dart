82614b56f14aff5e7d5f4fabee1ade58789f35e9
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-19 11:40:30 +0600

    init commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,14 +1,29 @@
 
- void main(){
+ import 'dart:io';
 
- var year=2024;
+void main(){
 
-if((year%4==0 )&& (year%100!=0 || year%400==0 )){
-
- print("leap year");
-}
+ var score=50;
+ 
+ if(score>80){
+  print("You got A+ ");
+ }
+ else if(score>70){
+  print("You got B+");
+ }
+ else if(score>60){
+  print("You got A-");
+ }
+ else if(score>50){
+  print("You got B-");
+ }
+ else if(score>40){
+  print("You got C+");
+ }
+ else if(score>33){
+  print("You got D+");
+ }
 else{
- print("Not leap year");
-}
-
+ print("You are Fail");
+ }
  }

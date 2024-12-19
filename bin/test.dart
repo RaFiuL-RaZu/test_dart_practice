ee64c339db5a3005cf78485fa42a7697c8d7762b
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-19 10:30:30 +0600

    init commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,19 +1,7 @@
 
  void main(){
 
-var productList=[
-  {'product':'Soap','price':100},
-  {'product':'Sugar','price':120},
-  {'product':'milk','price':80},
-  {'product':'sempo','price':200},
-  {'product':'potato','price':70},
-  {'product':'rice','price':50},
-  {'product':'biscuit','price':40},
- ];
- for(var oneproduct in productList){
-
-  var item="Product Name is ${oneproduct['product']} and Pirce is ${oneproduct['price']} taka";
-  print(item);
-  }
+ var name='Razu';
+ print(name);
 
  }

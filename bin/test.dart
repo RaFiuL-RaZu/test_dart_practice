c51e869a300902f91609fc2debac54432e456f5b
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-19 18:12:13 +0600

    fisrt commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,17 +1,10 @@
 
  import 'dart:io';
 
-void main(){
+ void main(){
 
- var num=40;
-
- if((num%5==0) && (num%7==0)){
-  print("Divisible");
- }
- else{
-  print("NOT divisible");
- }
- 
-
-
+  var i;
+  for(i=0; i<10;i++){
+   print(i);
+  }
  }

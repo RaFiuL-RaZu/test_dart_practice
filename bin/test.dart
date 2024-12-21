b65d25148a07adb3800773a3b05ecc0ecff08a61
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-21 20:17:45 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,8 +1,23 @@
-void main(){
-  int ? a=10;
 
-  if(a!=null){
-    int b=a+10;
-    print(b.floor());
+class MyClass{
+
+  var name="Rafiul Islam";
+  var alphabet=['A','B','C'];
+
+  addTwoNumber(int x,int y){
+    print(x+y);
   }
+  addThreeNumber(int x, int y, int z){
+  print(x+y+z);
+  }
+}
+
+void main(){
+  var obj=new MyClass();
+ obj.addTwoNumber(10, 20);
+ obj.addThreeNumber(10, 20, 30);
+
+ print(obj.name);
+ print(obj.alphabet);
+
 }

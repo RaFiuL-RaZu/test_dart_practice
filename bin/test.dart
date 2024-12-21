ca2213f8660a8430a1f7c475c472812c1d77823b
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-21 20:23:06 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,16 +1,4 @@
-
-class MyClass{
-
-  var name="Rafiul Islam";
-  var alphabet=['A','B','C'];
-
-  addTwoNumber(int x,int y){
-    print(x+y);
-  }
-  addThreeNumber(int x, int y, int z){
-  print(x+y+z);
-  }
-}
+import 'dart-practice.dart';
 
 void main(){
   var obj=new MyClass();

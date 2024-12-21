c8c93c23652eb677ff7ced3128db5ce4236e6fd3
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-21 20:33:40 +0600

    innit commit

--- bin/dart-practice.dart
+++ bin/dart-practice.dart
@@ -6,7 +6,7 @@ class MyClass{
   static addTwoNumber(int x,int y){
     print(x+y);
   }
-  addThreeNumber(int x, int y, int z){
+ static addThreeNumber(int x, int y, int z){
     print(x+y+z);
   }
 }
--- bin/test.dart
+++ bin/test.dart
@@ -3,7 +3,7 @@ import 'dart-practice.dart';
 void main(){
   var obj=new MyClass();
 
- obj.addThreeNumber(10, 20, 30);
+ MyClass.addThreeNumber(10, 20, 30);
 
  print(obj.name);
  print(obj.alphabet);

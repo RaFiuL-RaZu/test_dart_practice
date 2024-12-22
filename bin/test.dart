0e352290a17bf31297055bb1aa5073e9e6e2fe37
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-22 21:54:30 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,16 +1,8 @@
 void main(){
-  var x=10;
-  var y=20;
 
-  var add=x+y;
-  var sub=y-x;
-  var mul=x*y;
-  var div=y/x;
-  var mod=y/x;
+ var num=45;
 
-  print(add);
-  print(sub);
-  print(mul);
-  print(div);
-  print(mod);
+ if(num<40){
+   print("Number is smaller than 40.");
+ }
 }

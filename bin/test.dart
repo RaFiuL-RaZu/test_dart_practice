0f922e4c136ad5d43a1e3727a3b5e1a62e013299
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-23 18:16:23 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,9 +1,8 @@
 void main(){
 
- String string="This is Gen Z Bro";
  String str1="Coding is Fun";
  String str2=" Nothing is Done";
 
- print(string);
- print(str1+ str2);
+ bool val=str1==str2;
+ print(val);
 }

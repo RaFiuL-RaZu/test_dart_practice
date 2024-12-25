031f4f116d62f011fe1f0089bbd08df7a68fbff9
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-25 20:58:36 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -3,8 +3,8 @@ void main(){
   var ages={'Razu':24,'Sazu':18,'Rafi':25};
   
 
-   var name=ages.containsKey('Razu');
-   var age=ages.containsValue(24);
+   bool name=ages.containsKey('Razu');
+   bool age=ages.containsValue(24);
 
    print(name);
    print(age);

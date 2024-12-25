cb7a0dcaad01386a306b65d5c34e4dd22d05f5b0
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-25 18:08:58 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -2,8 +2,11 @@ void main(){
 
   var ages={'Razu':24,'Sazu':18,'Rafi':25};
   
- ages.forEach((key,value){
-   print('$key is $value years old');
-  });
+
+   var name=ages.keys.toList();
+   var age=ages.values.toList();
+
+   print(name);
+   print(age);
 
 }

56e833af3618db1faa26370fc9ded332f913a896
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-25 17:41:57 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,7 +1,7 @@
 void main(){
    var numbers=[1,2,3,4,5,6,7,88];
    
-   var number=numbers.where((number)=>number.isOdd).toList();
+   var number=numbers.reduce((razu,sazu)=> razu>sazu? razu:sazu);
 
 
 

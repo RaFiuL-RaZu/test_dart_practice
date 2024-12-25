6a64c07e151fe610fc687671611cd936656eb75b
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-25 16:23:17 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,8 +1,8 @@
 void main(){
 
- var name1= <String> {'This is for you'};
- print(name1);
- var name2=<String>{'This is our'};
- print(name2);
+ var map= {'Name':'Rafiul','Age':24,'Brand':'Computer'
+};
+
+ print(map);
 
 }

5845193d7c23ebb3209719fa9814fd8018dbd204
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-25 17:21:31 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,7 +1,9 @@
 void main(){
    var numbers=[1,2,3,4,5,6];
    
-   var number=numbers.where((number)=>number.isInfinite).toList();
+   var number=numbers.reduce((curr, next)=> curr>next? curr:next);
+
+
 
    print(number);
 }

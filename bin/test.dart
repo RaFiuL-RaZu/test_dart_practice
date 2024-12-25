ebf47fa904772f82d46ba565bf5a4ca2f9be05fa
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-25 17:47:10 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -2,5 +2,8 @@ void main(){
    var numbers=[10,21,3,41,5,6,7,88];
    //assending order
    numbers.sort();
+print(numbers);
+   //desending order
+   numbers.sort((a,b)=>b.compareTo(a));
    print(numbers);
 }

9d8c61bbe7855949fa4fce8e64b4e68711337392
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-26 11:08:42 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,12 +1,5 @@
-int sumOfElements(List<int> numbers) {
-  int sum = 0;
-  for (int number in numbers) {
-    sum += number;
-  }
-  return sum;
-}
-
 void main() {
-  List<int> numbers = [1, 2, 3, 4, 5];
-  print("The sum of the elements is: ${sumOfElements(numbers)}");
+  var numbers = [1, 2, 2, 3, 4, 5, 6, 5, 8];
+  var number=numbers.toSet().toList();
+  print(number);
 }

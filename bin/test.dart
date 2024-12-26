dd7e689434f25fa131db221a6f8413dd1ce53abc
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-26 11:46:45 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,7 +1,11 @@
 void main() {
   var map = {'Razu': 24, 'Rafi': 21,'Raz':18};
 
-  var filtermap=map..removeWhere((key,value)=> value<20);
-  print(filtermap);
+
+  var name=map.values.toList();
+  var age=map.keys.toList();
+
+  print(name);
+  print(age);
 
 }

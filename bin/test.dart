1125192b358a280f5185f37e379a19a6d09a2623
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-26 11:18:14 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,7 +1,7 @@
 void main() {
-  var numbers = [1, 2, 2, 3, 4, 5, 6, 5, 8];
-  var number=numbers.toSet().toList();
-  var evenNumbers=numbers.where((n)=>n%2==0).toList();
-  print(number);
-  print(evenNumbers);
+  var list1 = [1, 2, 2, 3];
+  var list2=  [4, 5, 6, 5, 8];
+  var marge= [...list1,...list2];
+
+  print(marge);
 }

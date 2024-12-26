ac65165b4ffa84218f206c2477714183a2e7c97d
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-26 11:21:00 +0600

    innit commit

--- bin/test.dart
+++ bin/test.dart
@@ -1,7 +1,6 @@
 void main() {
-  var list1 = [1, 2, 2, 3];
-  var list2=  [4, 5, 6, 5, 8];
-  var marge= [...list1,...list2];
+  var list1 = [1, 3, 2, 5,4,7,6];
 
-  print(marge);
+  list1.sort();
+  print(list1);
 }
